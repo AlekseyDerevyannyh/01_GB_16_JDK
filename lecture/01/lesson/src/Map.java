a40e89f9a03939363374109c97e377c495335a16
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-09-26 21:16:58 +0300

    modify Lecture 1

--- lecture/01/lesson/src/Map.java
+++ lecture/01/lesson/src/Map.java
@@ -3,10 +3,23 @@ import java.awt.*;
 
 public class Map extends JPanel {
     Map() {
-        setBackground(Color.BLACK);
+
     }
 
     void startNewGame(int mode, int fSzX, int fSzY, int wLen) {
         System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWin Length: %d", mode, fSzX, fSzY, wLen);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render(g);
+    }
+
+    private void render(Graphics g) {
+        g.setColor(Color.BLACK);
+        g.drawLine(0, 0, 100, 100);
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
     }
 }
