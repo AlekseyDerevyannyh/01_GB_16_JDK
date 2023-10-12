be75e1102da1bbe8cf8f5369b6d95bd0e0f79928
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-10-12 12:14:23 +0300

    init Seminar 3

---
+++ seminar/03/lesson/src/FirstClass.java
@@ -0,0 +1,21 @@
+import java.io.DataInput;
+import java.io.Serializable;
+
+public class FirstClass<T extends Comparable, V extends DataInput, K extends Number> {
+    T comparableItem;
+    V dataItem;
+    K numberItem;
+
+    FirstClass(T comparableItem, V dataItem, K numberItem) {
+        this.comparableItem = comparableItem;
+        this.dataItem = dataItem;
+        this.numberItem = numberItem;
+    }
+
+    void getNameClass() {
+        System.out.println(comparableItem.getClass().getName());
+        System.out.println(dataItem.getClass().getName());
+        System.out.println(numberItem.getClass().getName());
+    }
+
+}
---
+++ seminar/03/lesson/src/Main.java
@@ -0,0 +1,22 @@
+/*
+Создать обобщенный класс с тремя параметрами (T, V, K). Класс содержит три переменные типа (T, V, K), конструктор,
+принимающий на вход параметры типа (T, V, K), методы возвращающие значения трех переменных. Создать метод, выводящий
+на консоль имена классов для трех переменных класса. Наложить ограничения на параметры типа: T должен реализовать
+интерфейс Comparable (классы оболочки, String), V должен реализовать интерфейс DataInput и расширять класс InputStream,
+K должен расширять класс Number.
+*/
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        InputStream inputStream = new DataInputStream(new FileInputStream("test.txt"));
+
+        FirstClass<String, DataInputStream, Integer> firstClass = new FirstClass<>("any", new DataInputStream(inputStream), 3);
+        firstClass.getNameClass();
+    }
+}
