a87dbc76444523aed9a5e93222a97099271edf5b
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-10-18 23:08:16 +0300

    add Seminar 4 task4

---
+++ seminar/04/lesson/src/task4/Employee.java
@@ -0,0 +1,44 @@
+package task4;
+
+import java.time.LocalDateTime;
+
+public class Employee {
+    private static int idCounter = 0;
+    private int id;
+    private String phone;
+    private String name;
+    private LocalDateTime dateStartWorking;
+
+    public Employee(String phone, String name, LocalDateTime dateStartWorking) {
+        this.id = ++idCounter;
+        this.phone = phone;
+        this.name = name;
+        this.dateStartWorking = dateStartWorking;
+    }
+
+    public int getExperience() {
+        return LocalDateTime.now().getYear() - dateStartWorking.getYear();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", phone='" + phone + '\'' +
+                ", name='" + name + '\'' +
+                ", dateStartWorking=" + dateStartWorking +
+                '}';
+    }
+}
---
+++ seminar/04/lesson/src/task4/NoteEmployee.java
@@ -0,0 +1,176 @@
+package task4;
+
+import java.util.*;
+
+public class NoteEmployee implements List<Employee> {
+
+    private Employee[] employees;
+    private int index = 0;
+    private int initialCapacity = 100;
+
+    public NoteEmployee(int capacity) {
+        this.employees = new Employee[capacity];
+    }
+
+    public NoteEmployee() {
+        this.employees = new Employee[initialCapacity];
+    }
+
+    public List<Employee> getEmployeeByExperience(int workingYear) {
+        List<Employee> result = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee != null) {
+                if (employee.getExperience() == workingYear) {
+                    result.add(employee);
+                }
+            }
+        }
+        return result;
+    }
+
+    public Map<String, String> getPhoneByName(String name) {
+        Map<String, String> result = new HashMap<>();
+        for (Employee employee : employees) {
+            if (employee != null) {
+                if (employee.getName().equals(name)) {
+                    result.put(employee.getPhone(), employee.getName());
+                }
+            }
+        }
+        return result;
+    }
+
+    public Employee getEmployeeById(int id) {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                if (employee.getId() == id) {
+                    return employee;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Employee> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Employee employee) {
+        employees[this.index++] = employee;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Employee> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Employee> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Employee get(int index) {
+        return null;
+    }
+
+    @Override
+    public Employee set(int index, Employee element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Employee element) {
+
+    }
+
+    @Override
+    public Employee remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Employee> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Employee> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "NoteEmployee{" +
+                "employees=" + Arrays.toString(employees) +
+                '}';
+    }
+}
