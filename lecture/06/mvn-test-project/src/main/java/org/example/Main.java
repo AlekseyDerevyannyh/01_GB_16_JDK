6b9881364c0ccc9d4999690dc38ce8f0f24c7d7c
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-11-15 21:19:03 +0300

    modify Lecture 6

--- lecture/06/mvn-test-project/src/main/java/org/example/Main.java
+++ lecture/06/mvn-test-project/src/main/java/org/example/Main.java
@@ -1,8 +1,17 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public void printFile() throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream("hello.txt");
+        System.out.println(new String(inputStream.readAllBytes()));
+
+    }
+    public static void main(String[] args) throws IOException {
+        new Main().printFile();
     }
 
 }
