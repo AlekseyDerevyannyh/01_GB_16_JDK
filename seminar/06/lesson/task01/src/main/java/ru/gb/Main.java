151be26a6698b162e6c86f944d9407169194c8c7
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-11-16 23:02:20 +0300

    add Seminar 6 task01

--- seminar/06/lesson/task01/src/main/java/ru/gb/Main.java
+++ seminar/06/lesson/task01/src/main/java/ru/gb/Main.java
@@ -1,7 +1,31 @@
+/*
+Создать свой Java Maven проект;
+Добавьте зависимость commons-math3 (предназначена для решения математических задач) и изучить интерфейс библиотеки.
+Воспользоваться пакетом org.apache.commons.math3.stat и классом DescriptiveStatistics.
+Создать коллекцию из числовых элементов.
+С помощью объекта DescriptiveStatistics вычислить минимальное и максимальное значение, сумму и среднее арифметическое.
+Воспользоваться пакетом org.apache.commons.math3.util. С помощью класса ArithmeticUtils найти :
+факториал числа N.
+Наименьшее общее частное двух чисел
+Наибольший общий делитель двух чисел
+Проверить что число N это степень двойки
+ */
 package ru.gb;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.util.ArithmeticUtils;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] doubleArray = new double[]{3.1, 3.2, 3.14};
+        DescriptiveStatistics ds = new DescriptiveStatistics(doubleArray);
+        System.out.println(ds.getMin());
+        System.out.println(ds.getMax());
+        System.out.println(ds.getSum());
+        System.out.println(ds.getMean());
+        System.out.println(ArithmeticUtils.factorial(5));
+        System.out.println(ArithmeticUtils.lcm(27, 18));
+        System.out.println(ArithmeticUtils.gcd(27, 18));
+        System.out.println(ArithmeticUtils.isPowerOfTwo(5));
     }
 }
