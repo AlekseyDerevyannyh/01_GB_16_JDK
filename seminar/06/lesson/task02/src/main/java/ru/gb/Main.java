c7a6d942980c31223eddba6a9beb562fe9ad6130
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-11-17 17:32:26 +0300

    modify Seminar 6 task02

--- seminar/06/lesson/task02/src/main/java/ru/gb/Main.java
+++ seminar/06/lesson/task02/src/main/java/ru/gb/Main.java
@@ -9,21 +9,18 @@
 Получить итоговый Set из двух коллекций
 Получить итоговый Set из общих элементов двух коллекций
 Получить итоговый Set из непересекающихся элементов двух коллекций
-Создать свой Java Gradle проект;
-Добавить зависимость Guava (популярная библиотека от Google, содержащая набор утилитарных механизмов).
-Воспользоваться утилитарным классом Lists:
-Развернуть список элементов
-Получить лист Character из строки
-Разделить лист на группы по 2 элемента
-Воспользоваться утилитарным классом Sets
-Получить итоговый Set из двух коллекций
-Получить итоговый Set из общих элементов двух коллекций
-Получить итоговый Set из непересекающихся элементов двух коллекций
  */
 package ru.gb;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Character> list = Lists.charactersOf("abcdef");
+        System.out.println(list);
+        List<List<Character>> list1 = Lists.partition(list, 2);
+        System.out.println(list1);
     }
 }
