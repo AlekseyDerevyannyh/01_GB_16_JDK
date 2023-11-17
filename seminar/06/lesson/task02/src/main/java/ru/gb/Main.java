8aa8b5791676ccf841849548be9b1699587bfd9f
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-11-17 20:26:31 +0300

    add Seminar 6 task02

--- seminar/06/lesson/task02/src/main/java/ru/gb/Main.java
+++ seminar/06/lesson/task02/src/main/java/ru/gb/Main.java
@@ -13,14 +13,24 @@
 package ru.gb;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         List<Character> list = Lists.charactersOf("abcdef");
+        List<Character> list2 = Lists.charactersOf("defghij");
         System.out.println(list);
         List<List<Character>> list1 = Lists.partition(list, 2);
         System.out.println(list1);
+
+        Set<Character> set1 = new HashSet<>(list);
+        Set<Character> set2 = new HashSet<>(list2);
+        System.out.println(Sets.union(set1, set2));
+        System.out.println(Sets.intersection(set1, set2));
+        System.out.println(Sets.symmetricDifference(set1, set2));
     }
 }
