4ca813c9f3b65f0e31fd70993c1a7cdb7bb9d606
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2023-11-19 23:33:55 +0300

    modify HW 6

---
+++ seminar/06/hw/src/main/java/ru/gb/Main.java
@@ -0,0 +1,14 @@
+package ru.gb;
+
+import com.google.common.collect.Sets;
+
+import java.util.Random;
+
+public class Main {
+    public static final int COUNTS = 1000;
+    public static Random rnd = new Random();
+
+    public static void main(String[] args) {
+        Sets.
+    }
+}
---
+++ seminar/06/hw/src/main/java/ru/gb/Monty.java
@@ -0,0 +1,6 @@
+package ru.gb;
+
+public class Monty {
+
+
+}
